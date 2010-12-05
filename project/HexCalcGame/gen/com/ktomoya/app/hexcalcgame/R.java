ed603205aacab084417de3063fbf3f79a55d1b97
hi32ce <hi32ce_2@hotmail.com>
2010-12-06 03:42:22 +0900

    preference test now

--- project/HexCalcGame/gen/com/ktomoya/app/hexcalcgame/R.java
+++ project/HexCalcGame/gen/com/ktomoya/app/hexcalcgame/R.java
@@ -8,12 +8,34 @@
 package com.ktomoya.app.hexcalcgame;
 
 public final class R {
+    public static final class array {
+        public static final int difficulty_name_list=0x7f040000;
+        public static final int difficulty_value_list=0x7f040001;
+    }
     public static final class attr {
     }
+    public static final class id {
+        public static final int Button01=0x7f060003;
+        public static final int difficulty_easy=0x7f060000;
+        public static final int difficulty_hard=0x7f060002;
+        public static final int difficulty_normal=0x7f060001;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int hexcalcgameactivity=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f050000;
+        public static final int game_rank=0x7f050002;
+        public static final int game_result=0x7f050001;
+        public static final int menu_preference=0x7f050005;
+        public static final int menu_rank=0x7f050006;
+        public static final int pre_difficulty=0x7f050004;
+        public static final int pre_difficulty_easy=0x7f050007;
+        public static final int pre_difficulty_hard=0x7f050009;
+        public static final int pre_difficulty_normal=0x7f050008;
+        public static final int start_button=0x7f050003;
+    }
+    public static final class xml {
+        public static final int preference=0x7f030000;
     }
 }
