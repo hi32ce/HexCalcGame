80686bd33edd0be76de432c6a3c110c52fa9552a
hi32ce <hi32ce_2@hotmail.com>
2010-12-12 01:35:13 +0900

    fix button enable

--- project/HexCalcGame/src/com/ktomoya/app/hexcalcgame/HexCalcGameActivity.java
+++ project/HexCalcGame/src/com/ktomoya/app/hexcalcgame/HexCalcGameActivity.java
@@ -8,23 +8,29 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class HexCalcGameActivity extends Activity {
+	private Button mStartButton;
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {	
-    	Button startButton;
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hexcalcgameactivity);
         
-        startButton = (Button)findViewById(R.id.StartButton);
-        startButton.setOnClickListener(new OnClickListener(){
+        mStartButton = (Button)findViewById(R.id.StartButton);
+        mStartButton.setOnClickListener(new OnClickListener() {
         	@Override
         	public void onClick(View v) {
+        		v.setEnabled(false);
         		Intent intent = new Intent();
         		intent.setClass(HexCalcGameActivity.this, GameActivity.class);
         		startActivity(intent);
         		finish();
         	}
         });
+    }
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	mStartButton.setEnabled(true);
     }
 }
 
